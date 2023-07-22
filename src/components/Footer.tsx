a9601cf69c7f02ed3ada7fcf9259caa5c439612f
ctasbihas <ctasbihas@gmail.com>
2023-07-22 11:30:35 +0600

    Adding Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            Footer.
+        <div className="flex items-center justify-between p-4 lg:px-20 xl:px-40 text-red-500 h-12 md:h-24 border-t-2 border-t-red-500">
+            <Link href="/" className="font-bold text-xl" >Food Express Go</Link>
+            <p>&copy;All Rights Reserved.</p>
         </div>
     );
 };
